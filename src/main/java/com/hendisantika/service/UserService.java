72fc26c0e8fdfe6dbf8d27b2369ab49ac7762782
Hendi Santika <hendisantika@gmail.com>
2021-10-03 08:47:59 +0700

    #9 - Add getOneUser in UserService

--- src/main/java/com/hendisantika/service/UserService.java
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +28,9 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> getOneUser(Integer id) {
+        return userRepository.findById(id);
     }
 }
