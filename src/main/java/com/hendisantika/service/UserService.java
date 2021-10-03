3e4ced45987200cb297d2268f426e85739b2fd35
Hendi Santika <hendisantika@gmail.com>
2021-10-03 08:47:37 +0700

    #8 - Add getAllUsers in UserService

--- src/main/java/com/hendisantika/service/UserService.java
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -5,6 +5,8 @@ import com.hendisantika.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-recaptcha-demo1
@@ -21,5 +23,9 @@ public class UserService {
 
     public void createUser(User user) {
         userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
